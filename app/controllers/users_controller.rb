6c278f3c5739535cf012b514773ad11e195ebfb7
yasushiasahi <asahi1600@gmail.com>
2017-06-17 01:41:15 +0000

    add nickname column, add user edit destroy

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -24,11 +24,38 @@ class UsersController < ApplicationController
       render :new
     end
   end
+  
+  def edit
+    @user = User.find(session[:user_id])
+  end
+  
+  def update
+    @user = User.find(session[:user_id])
+    
+    if @user.update(user_update_params)
+      flash[:success] = "プロフィールを更新しました。"
+      redirect_to @user
+    else 
+      flash[:danger] = "プロフィールの更新に失敗しました。"
+      render :edit
+    end
+  end
+  
+  def destroy
+    @user = User.find(session[:user_id])
+    @user.destroy
+    flash[:success] = "アカウント削除しました"
+    redirect_to "/"
+  end
 
   private
   
   def user_params
-    params.require(:user).permit(:name, :email, :password, :password_confirmation)
+    params.require(:user).permit(:name, :email, :password, :password_confirmation, :nickname)
   end
-    
+  
+  def user_update_params
+    params.require(:user).permit(:name, :email, :nickname)
+  end
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Users.create(name: "名前テスト",
+                email: "test@test.test",
+                password_digest: "test")
