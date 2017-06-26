0a8f0d28b9d72f8649e0eb14beb9c4bcc6b762ee
yasushiasahi <asahi1600@gmail.com>
2017-06-26 09:01:21 +0000

    follow, unfollow

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,13 @@ Rails.application.routes.draw do
   delete "logout", to: "sessions#destroy"
   
   get "signup", to: "users#new"
-  resources :users
+  resources :users do
+    member do
+      get :followings
+      get :followers
+    end
+  end
   
   resources :microposts, only: [:create, :destroy]
+  resources :relationships, only: [:create, :destroy]
 end
